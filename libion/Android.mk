93b60c9573ac84c7b640619404f7f61bdf2ee9fa
Sundar Raman <a0393242@ti.com>
2014-04-07 11:55:28 -0500

    ion: test: add ion_test_2 for testing TI custom IOCTLS
    TI specific test for testing custom extensions to ION
    inside ti_ion.c
    
    Change-Id: I916280f819fce8f70165ac1e05ca51d3c2805667
    Signed-off-by: Sundar Raman <a0393242@ti.com>
    Signed-off-by: Sunita Nadampalli <sunitan@ti.com>

--- libion/Android.mk
+++ libion/Android.mk
@@ -21,3 +21,12 @@ LOCAL_MODULE := iontest
 LOCAL_MODULE_TAGS := optional tests
 LOCAL_SHARED_LIBRARIES := liblog
 include $(BUILD_EXECUTABLE)
+
+ifdef OMAP_ENHANCEMENT
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := ion.c ion_ti.c ion_test_2.c
+LOCAL_MODULE := ion_ti_test2
+LOCAL_MODULE_TAGS := optional tests
+LOCAL_SHARED_LIBRARIES := liblog
+include $(BUILD_EXECUTABLE)
+endif
